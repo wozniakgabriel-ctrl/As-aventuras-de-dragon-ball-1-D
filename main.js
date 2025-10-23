cf41ddabcec21e0243411be1cbbe92d4f68cbaba
wozniakgabriel-ctrl <wozniak.gabriel@escola.pr.gov.br>
2025-10-23 10:24:48 -0300

    ok

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ console.log(avanca)
 avanca.forEach(button) => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo')
-    })
+     const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+   
+    atual.classList.remove('ativo')
+    document.getElementById(proximoPasso).classList.add('data-proximo')
+     
+  })
 })
